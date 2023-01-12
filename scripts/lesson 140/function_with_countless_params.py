76fe5ab123d9c41ea4337074086be81040b5b3b4
Vasilev <Martin.Vasilev@adastragrp.com>
2023-01-12 14:56:08 +0200

    lesson 140 task 1 updated

--- scripts/lesson 140/function_with_countless_params.py
+++ scripts/lesson 140/function_with_countless_params.py
@@ -1,15 +1,18 @@
 '''
 -Write a function which takes any number of parameters and returns their average. 
 '''
-def get_avg(*args):
+
+'''def get_avg_v1(*args):
     sum = 0
     count = 0
 
     for i in args:
         sum += i
         count += 1
-    return sum * 1.0 / count
+    return sum * 1.0 / count'''
 
+def get_avg_v2(*args):
+    return sum(args) / len(args)  
 
-x = get_avg(1, 2, 3, 4, 5, 6, 10)
+x = get_avg_v2(1, 2, 3, 4, 5, 6, 10)
 print(x)
