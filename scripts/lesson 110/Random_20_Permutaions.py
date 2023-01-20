ba4ddd5c128038efce12248dfdac9e8e6fcd3444
Vasilev <Martin.Vasilev@adastragrp.com>
2023-01-20 17:05:45 +0200

    task in lesson 110 fixed

--- scripts/lesson 110/Random_20_Permutaions.py
+++ scripts/lesson 110/Random_20_Permutaions.py
@@ -1,19 +1,27 @@
 import sys
+from functools import cache
 import random
 
-results = []
+num = random.randint(15, 20)
+count = 0
+percent_chance = 99.9
 
-def generate_permutaions(a, n):
+@cache
+def generate_permutations(a, n):
+
+    global count
 
     if n == 0:
-        #print(''.join(a))
-        results.append(''.join(a))
+        if not count == num and random.uniform(0.0, 100.0) > percent_chance:
+            print(''.join(a))
+            count += 1
     else:
         for i in range(n):
-            generate_permutaions(a, n - 1)
+            generate_permutations(a, n - 1)
             j = 0 if n % 2 == 0 else i
-            a[j], a[n] = a[n], a[j]
-        generate_permutaions(a, n - 1)
+            b = list(a)
+            b[j], b[n] = b[n], b[j]
+        generate_permutations(tuple(b), n - 1)
 
 if len(sys.argv) != 2:
     sys.stderr.write('Exactly one argument is required\n')
@@ -21,13 +29,4 @@ if len(sys.argv) != 2:
 
 word = sys.argv[1]
 
-generate_permutaions(list(word), len(word) - 1)
-
-randoms = []
-size = 20
-
-for i in range(0, size):
-    randoms.append(random.randint(0, len(results)))
-
-for i in range(0, len(randoms)):
-    print(results[randoms[i]])
+generate_permutations(tuple(list(word)), len(word) - 1)
